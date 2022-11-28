a94fc6735ffefcc2decf7730ca452865c8f21f24
Tuana Özgü <s.tuozgu@gmail.com>
2022-11-28 20:08:04 +0300

    Level Manager

---
+++ Assets/Scripts/Commands/OnLevelDestroyerCommand.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnLevelDestroyerCommand : ICommand
+{
+    private Transform _levelHolder;
+
+
+
+    public OnLevelDestroyerCommand(Transform levelHolder)
+    {
+        _levelHolder = levelHolder;
+    }
+    
+    
+    public void Execute()
+    {
+        Object.Destroy(_levelHolder.GetChild(0).gameObject);
+    }
+
+    public void Execute(int value)
+    { 
+
+    }
+}
---
+++ Assets/Scripts/Signals/CoreGameSignals.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CoreGameSignals : MonoBehaviour
+{
+    #region Singleton
+    // Static, class silinse veya bulunduğu object yok edilse bile işlemeye devam etmesine yarar. 
+    public static CoreGameSignals Instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        Debug.LogWarning(Instance.GetInstanceID().ToString());
+    }
+
+    #endregion
+
+    public UnityAction<GameStates> onChangeGameState = delegate { };
+    public UnityAction<int> onLevelInitialize = delegate { };
+    public UnityAction onClearActiveLevel = delegate { };
+    public UnityAction onLevelFailed = delegate { };
+    public UnityAction onLevelSuccessful = delegate { };
+    public UnityAction onNextLevel = delegate { };
+    public UnityAction onRestartLevel = delegate { };
+    public UnityAction onReset = delegate { };
+
+}
