0517892c16fffb0c689809710e95589e54e2291d
Tuana Özgü <s.tuozgu@gmail.com>
2023-01-07 17:45:08 +0300

    Restart from Level Manager
    Due to some bugs and errors about GitHub, I deleted all contents in the folders and restarted to commit from first Level Manager commit.

--- Assets/Scripts/Enums/UIEventSubscriptionTypes.cs
+++
@@ -1,6 +0,0 @@
-public enum UIEventSubscriptionTypes
-{
-    OnPlay,
-    OnNextLevel,
-    OnRestartLevel,
-}
--- Assets/Scripts/Enums/UIPanelTypes.cs
+++
@@ -1,7 +0,0 @@
-﻿public enum UIPanelTypes 
-{
-    Start,
-    Win,
-    Fail,
-    Level
-}
